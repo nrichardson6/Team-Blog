26908af7d90e19eaec0fe0f64c53fbdc20f03121
sjohnston92 <simonjohnston.c@gmail.com>
2020-09-17 15:54:45 -0600

    Too many cooks will spoil the Broth

--- src/Simon.js
+++ src/Simon.js
@@ -1,10 +1,30 @@
 
 import React from "react";
+import { Header, } from "semantic-ui-react";
+import Cook from "./Scook"
 
+class Simon extends React.Component {
+  state = {
+    cooks: [
+      { id: 1, name: "Cooks", phrase: "Too many Cooks" },
+      { id: 2, name: "Kooks", phrase: "Dont spoil the broth" },
+      { id: 3, name: "Cook", phrase: "Family is like a soup" },
+    ]
+  };
 
-
-const Simon = () => (
-  <h1>Hey its Simon</h1>
-);
+  
+  renderCooks = () => {
+    return this.state.cooks.map( cook => <Cook key={cook.id} {...cook} />)
+  };
+  
+  render() {
+    return (
+      <div>
+        <Header as="h1">It takes alot to make a Stew.....</Header>
+        { this.renderCooks() }
+      </div>
+    )
+  }
+}
 
 export default Simon;
