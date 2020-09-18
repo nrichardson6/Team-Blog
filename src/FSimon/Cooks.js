3bbf3d091fd8de1de5fe76c436d0a84d5e172c42
sjohnston92 <simonjohnston.c@gmail.com>
2020-09-18 16:40:06 -0600

    Display working on the Cooks Function

--- src/FSimon/Cooks.js
+++ src/FSimon/Cooks.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 const Cooks = () => {
+
+  const [cooks,setCooks] = useState([
+    { name:"Mr.Cook", phrase:"Don't Spoil the broth Dear"},
+    { name:"Mrs.Cook", phrase:"Family is like a Soup" },
+  ]);
+
+
+function renderCooks (){
+  return cooks.map(cook=> {
+    return(
+      <div>
+        {cook.name} Catch Phrase: {cook.phrase}
+      </div>
+    )
+
+});
+};
+
   return (
     <div>
       <h1>Cooks</h1>
+      {renderCooks()}
     </div>
   );
 };
