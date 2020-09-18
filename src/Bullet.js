2bf6fc0a5c28392544eba379df625f4b687fbc47
VidensKnowledge <devydoolander@gmail.com>
2020-09-18 09:25:57 -0600

    edit toggle working

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { Header, Segment, } from "semantic-ui-react";
+import { Header, Segment, Button, Icon, } from "semantic-ui-react";
+import BulletForm from "./BulletForm";
 
 
 class Bullet extends React.Component {
+  state = { editing: false,}; // Editing will apply a form function to the already declared bullet title and body. this will allow for updating.
+
+  toggleEdit = () => this.setState({
+    editing: !this.state.editing, // this changes the default "false" state of editing to true
+  });
+
+
+
   render() {
     return (
-      <Segment>
+      <Segment style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+      {
+        this.state.editing ?  <BulletForm { ...this.props } />  
+        : <div><Header as="h3">{this.props.title}</Header></div>
+      }
+      <div>
+        <Button icon color="blue" onClick={this.toggleEdit}>
+          <Icon name="pencil" />
+        </Button>
+        <Button icon color="blue" onClick={this.toggleEdit}>
+          <Icon name="trash" />
+        </Button>
+      </div>
         <Header as="h3">{ this.props.title }</Header>
         <p>{ this.props.body }</p>
       </Segment>
