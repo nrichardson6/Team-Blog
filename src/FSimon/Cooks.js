2155061a8ca6e9a88bfc3fa7fde47d0e403a2fe9
sjohnston92 <simonjohnston.c@gmail.com>
2020-09-18 17:15:44 -0600

    Added the Toggler in

--- src/FSimon/Cooks.js
+++ src/FSimon/Cooks.js
@@ -7,13 +7,21 @@ const Cooks = () => {
     { name:"Mr.Cook", phrase:"Don't Spoil the broth Dear"},
     { name:"Mrs.Cook", phrase:"Family is like a Soup" },
   ]);
+  const [showCooks,setShowCooks] = useState(true);
 
 
+
+
+
+
+
+//CANNOT USE CONST HERE
 function renderCooks (){
   return cooks.map(cook=> {
     return(
       <div>
-        {cook.name} Catch Phrase: {cook.phrase}
+        <h2>{cook.name}</h2><h3>Catch Phrase: {cook.phrase}</h3>
+        <br />
       </div>
     )
 
@@ -23,7 +31,12 @@ function renderCooks (){
   return (
     <div>
       <h1>Cooks</h1>
-      {renderCooks()}
+      <br />
+      <button onClick={() => setShowCooks(!showCooks)}>Cook Toggler</button>
+      <br />
+      <br />
+      <hr />
+      {showCooks && renderCooks()}
     </div>
   );
 };
